3824071532f8f9e543a73d03fcf9e5be9c43c7b8
David Adair <adair.david@gmail.com>
2013-07-17 11:36:26 -0600

    Basic Qt Window is shown.

--- src/main.cc
+++ src/main.cc
@@ -15,9 +15,15 @@
 
 #include <stdio.h>
 
-int main(int argc, const char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    printf("argv[%d]: %s\n", i, argv[i]);
-  }
-  return 0;
+#include <QtGui/QApplication>
+#include <QMainWindow>
+
+int main(int argc, char *argv[]) {
+  QApplication app(argc, argv);
+
+  QMainWindow window;
+  window.setWindowTitle("Hello World!");
+  window.show();
+
+  return app.exec();
 }
